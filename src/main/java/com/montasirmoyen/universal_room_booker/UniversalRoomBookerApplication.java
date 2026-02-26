52f08e719ddeca6e3589993c2c716917d5885f94
montasirmoyen <montasirmoyen@gmail.com>
2026-02-26 10:18:55 -0500

    Apply boilerplate with Maven config and Spring Boot setup

---
+++ src/main/java/com/montasirmoyen/universal_room_booker/UniversalRoomBookerApplication.java
@@ -0,0 +1,13 @@
+package com.montasirmoyen.universal_room_booker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversalRoomBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversalRoomBookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/montasirmoyen/universal_room_booker/UniversalRoomBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montasirmoyen.universal_room_booker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversalRoomBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
